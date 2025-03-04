02f8b99bd7ab8ce564237179e99ed07420a2da39
AhmedKabbej <ahmed.kabbej@edu.gobelins.fr>
2025-03-04 17:01:31 +0100

    mardi commit

--- src/main.js
+++ src/main.js
@@ -4,23 +4,32 @@ const socket = io( 'http://localhost:3000');
 
 
 
-
-
 export class Game {
   constructor(socket) {
     this.socket = socket;
     this.joinButton = document.querySelector('#joinButton');
     this.playerNameInput = document.querySelector('#playerNameInput');
     this.messageContainer = document.getElementById('messageContainer');
+    this.buttonReady = document.getElementById('ready');
+    this.playerList = document.getElementById('playerList');
 
+    
     // Initialisation des événements
-    this.initialize();
+    this.initEvents();
+    this.initSocketListener();
   }
 
   // Initialiser les événements
-  initialize() {
+  initEvents() {
     this.joinButton.addEventListener('click', this.handleJoinGame.bind(this));
+    this.buttonReady.addEventListener('click', this.handleButtonReady.bind(this));
+   
+  }
+ 
+
+  initSocketListener() {
     this.socket.on('joinGameStatus', this.handleJoinGameStatus.bind(this));
+    this.socket.on('updatePlayers', this.handleUpdatePlayers.bind(this));
   }
 
   // Méthode pour gérer le clic du bouton "Rejoindre le jeu"
@@ -28,8 +37,7 @@ export class Game {
     const playerName = this.playerNameInput.value;
     console.log('Player Name:', playerName);
 
-    // Changer la couleur de fond du bouton avec la classe 'button-3'
-    document.querySelector('.button-3').style.backgroundColor = 'green';
+   
 
     // Émettre l'événement pour rejoindre la partie
     this.socket.emit('joinGame', playerName);
@@ -44,10 +52,48 @@ export class Game {
       this.messageContainer.textContent = 'Message: ' + JSON.stringify(message); // Affiche l'objet entier
     }
   }
+  handleUpdatePlayers(players){
+    players.forEach(player => {
+      console.log(player.name);
+      console.log(player.isReady);
+      this.createPlayer(player);
+      
+    })
+
+      // console.log(players[0].name);
+  }
+  handleButtonReady(){
+    socket.emit('playerReady');
+     // Changer la couleur de fond du bouton avec la classe 'button-3'
+    document.querySelector('.button-3').style.backgroundColor = 'green';
+  }
+
+
+
+//cree un paragraphe pour ready et un pour le pseudo dans chaque li
+
+//ajouter chaque li au ul 'playerList' id
+
+createPlayer(player){
+  //recuperer pour chaque joueur ready et le pseudo
+  const li = document.createElement("li");
+  this.playerList.appendChild(li);
+  const pIsready = document.createElement("p");
+  li.appendChild(pIsready);
+  console.log(player.isReady);
+  pIsready.innerText(pIsready);
+
 }
+
+}
+
+
 
 // Initialisation du jeu avec le socket (vous devez définir le socket au préalable)
 const game = new Game(socket);
+
+
+
 
 
 
